21f782733af18c1390aee242f89770ec7387e5bf
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2019-08-31 13:35:41 +0300

    * abstract

--- src/test/java/ru/xpendence/cache/AbstractTest.java
+++ src/test/java/ru/xpendence/cache/AbstractTest.java
@@ -1,11 +1,10 @@
 package ru.xpendence.cache;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AbstractTest {
+public abstract class AbstractTest {
 }
